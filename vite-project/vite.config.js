1563a6144b7409e8d6b5f76ef66bd7193665cb22
Joelwine <stimdlastim@gmail.com>
2024-11-16 13:54:36 +0600

    feat: architecture

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      UI: '/src/UI',
+      assets: '/src/assets',
+      utils: '/src/utils',
+      modules: '/src/modules',
+      store: '/src/store',
+      routes: '/src/app/routes',
+      styles: '/src/app/styles',
+      layout: '/src/app/layout',
+      pages: '/src/pages',
+    },
+  },
+});
