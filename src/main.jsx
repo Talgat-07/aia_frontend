b8888603ccea05ece99f3f3fdf135af75b889488
Joelwine <stimdlastim@gmail.com>
2024-11-26 22:26:38 +0600

    feat:arch

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import {RouterProvider} from 'react-router-dom';
-import {Router} from "./routes/Router.jsx";
-
+import {Router} from "routes/Router.jsx";
 
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={Router}/>
--- src/modules/footer/Footer.jsx
+++ src/modules/footer/Footer.jsx
@@ -2,7 +2,7 @@
 export const Footer = () => {
     return (
         <div>
-            sdad
+            footer
         </div>
     );
 };
