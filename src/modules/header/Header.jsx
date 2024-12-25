d8461487b9bc65b1a4883614fbd1a1451287c554
Joelwine <stimdlastim@gmail.com>
2024-12-26 00:23:34 +0600

    typography

--- src/modules/header/Header.jsx
+++ src/modules/header/Header.jsx
@@ -1,12 +1,13 @@
 import {Typography} from "UI/typography/Typography.jsx";
 import 'UI/typography/Typography.module.scss'
 
-export const Header = () => {
+export const Header =  () => {
+
     return (
         <header>
-            <Typography style={{ fontFamily:"Noto Sans Bold"}} variant="h2" weight="bold" color="black">
-                The quick brown fox jumps over the lazy dog.
-            </Typography>
+           <Typography variant="h1" weight="semibold" color="black">
+               The quick brown fox jumps over the lazy dog.
+           </Typography>
         </header>
     );
 };
