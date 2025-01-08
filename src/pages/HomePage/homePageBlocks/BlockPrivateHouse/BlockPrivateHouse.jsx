1e5b489981dd5f8e8e9995220c3f60a93b21b1ec
AdinaMavlanbekova <100846189+AdinaMavlanbekova@users.noreply.github.com>
2025-01-08 15:36:20 +0600

    блок на главной странице проект частных домов, есть горизонтальный скролл(не должно быть)

--- src/pages/HomePage/homePageBlocks/BlockPrivateHouse/BlockPrivateHouse.jsx
+++ src/pages/HomePage/homePageBlocks/BlockPrivateHouse/BlockPrivateHouse.jsx
@@ -1,35 +1,64 @@
+import { useState, useRef } from 'react';
 import { CustomButton } from 'UI/buttons/CustomButton/CustomButton.jsx';
-import styles from './BlockIndiProekt.module.scss';
+import styles from './BlockPrivateHouse.module.scss';
 
-export const BlockIndiProekt = () => {
+export const BlockPrivateHouse = () => {
+
+   const [expanded, setExpanded] = useState(false);
+   const textRef = useRef(null);
+
+   const handleToggle = () => {
+      if (textRef.current) {
+         if (expanded) {
+            textRef.current.scrollTo({ top: 0, behavior: 'smooth' });
+         } else {
+            textRef.current.scrollTo({ top: textRef.current.scrollHeight, behavior: 'smooth' });
+         }
+      }
+      setExpanded(!expanded);
+   };
 
    return (
-      <div className={`${styles.container} ${styles.blockIndiProekt} `}>
-         <div className={styles.blockIndiProekt__title}>
-            <h2>Индивидуальное проектирование дома</h2>
-            <div className={styles.shortLine}>
+      <div className={styles.blockPrivateHouse}>
+         <div className={styles.container}>
+            <div className={styles.blockPrivateHouse__title}>
+               <h2>Проектирование частных домов</h2>
+               <div className={styles.shortLine}>
+
+               </div>
+            </div>
+
+            <div className={styles.blockPrivateHouse__lowerBlock}>
+               <div>
+                  <p ref={textRef}
+                     className={`${styles.textContent} ${expanded ? styles.expanded : ''}`}
+                  >
+                     Lorem ipsum dolor sit amet consectetur. Mi tristique risus accumsan morbi.
+                     Sed adipiscing libero vel enim justo phasellus a et. Placerat ultricies
+                     ultricies rutrum blandit et arcu. Feugiat arcu turpis interdum tortor eu ut
+                     diam tincidunt.Lorem ipsum dolor sit amet consectetur. Mi tristique
+                     risus accumsan morbi. Sed dipiscing libero vel.Lorem ipsum dolor sit amet
+                     consectetur. Mi tristique risus accumsan morbi. Sed adipiscing libero vel enim
+                     justo phasellus a et. Placerat ultricies ultricies rutrum blandit et arcu.
+                     Feugiat arcu turpis interdum tortor eu ut diam tincidunt.Lorem ipsum dolor sit ametdiam
+                     tincidunt.Lorem
+                  </p>
+                  <button className={styles.readMoreButton} onClick={handleToggle}>
+                     {expanded ? 'Скрыть' : 'Читать дальше'}
+                  </button>
+                  <div className={styles.blockPrivateHouse__buttonPosition}>
+                     <CustomButton buttonStyles="customButtonBrown" text={'Посмотреть проекты'} />
+                  </div>
+               </div>
+               <div className={styles.blockPrivateHouseImg}>
+                  <img
+                     src="https://forum.cs-cart.ru/uploads/default/original/2X/1/114e3184a8bfbd17c5d2d9b15cc3442767e0513b.jpg"
+                     alt="21" />
+               </div>
+            </div>
+            <div className={styles.wideLine}>
 
             </div>
-         </div>
-
-         <div className={styles.blockIndiProekt__lowerBlock}>
-            <div className={styles.indiProektImg}>
-               <img
-                  src="https://forum.cs-cart.ru/uploads/default/original/2X/1/114e3184a8bfbd17c5d2d9b15cc3442767e0513b.jpg"
-                  alt="21" />
-            </div>
-            <div>
-               <p>
-                  Lorem ipsum dolor sit amet consectetur. Mi tristique risus accumsan morbi.
-                  Sed adipiscing libero vel enim justo phasellus a et. Placerat ultricies
-                  ultricies rutrum blandit et arcu. Feugiat arcu turpis interdum tortor
-                  eu ut diam tincidunt.Lorem ipsum dolor sit amet
-               </p>
-               <CustomButton buttonStyles='customButtonBrown' text={'Заказать индивидуальный проект'} />
-            </div>
-         </div>
-         <div className={styles.wideLine}>
-
          </div>
       </div>
    )
