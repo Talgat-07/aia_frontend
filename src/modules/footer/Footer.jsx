ce03ca1f930ab5f27d406a8cf36afc5bfa086edc
Joelwine <stimdlastim@gmail.com>
2025-01-08 19:41:02 +0600

    fix

--- src/modules/footer/Footer.jsx
+++ src/modules/footer/Footer.jsx
@@ -1,9 +1,9 @@
 import styles from "./Footer.module.scss";
-import { FooterContacts } from "./components/FooterContacts/FooterContacts.jsx";
-import { FooterLinks } from "./components/FooterLinks/FooterLinks.jsx";
-import { FooterHours } from "./components/FooterHours/FooterHours.jsx";
-import { FooterSocial } from "./components/FooterSocial/FooterSocial.jsx";
-import { GeeksProCopyright } from "./components/GeeksProCopyright/GeeksProCopyright.jsx";
+import { FooterContacts } from "modules/footer/Components/FooterContacts/FooterContacts.jsx";
+import { FooterLinks } from "modules/footer/Components/FooterLinks/FooterLinks.jsx";
+import { FooterHours } from "modules/footer/Components/FooterHours/FooterHours.jsx";
+import { FooterSocial } from "modules/footer/Components/FooterSocial/FooterSocial.jsx";
+import { GeeksProCopyright } from "modules/footer/Components/GeeksProCopyright/GeeksProCopyright.jsx";
 
 export const Footer = () => {
     return (
