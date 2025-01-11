d0a19bcd343b9ff135a5bc0bdd926aa3403f1d42
Joelwine <stimdlastim@gmail.com>
2025-01-11 14:06:14 +0600

    fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,25 +1,9 @@
-import {MoreProjects} from "UI/moreProjects/moreProjects";
-import image from "assets/img/moreProjects.png";
-import { Heading } from "UI/heading/Heading";
+
 
 const HomePage = () => {
-  const initialConfig = {
-   image: image,
-   subtitle: 'Подзаголовок',
-    text: 'Lorem ipsum dolor sit amet consectetur. Mi tristique risus accumsan morbi. Sed adipiscing libero vel enim justo phasellus a et. Placerat ultricies ultricies rutrum blandit et arcu. Feugiat arcu turpis interdum tortom dolor sit amet consectetur. Mi tristique risus.',
-    buttons: [
-      'Реализованные проекты',
-      'Архитектурное проектирование',
-      'Дизайн общественных пространств',
-    ],
-  };
 
   return (
     <div>
-      HELLO
-      <MoreProjects initialConfig={initialConfig}>
-        <Heading text="Больше проектов" color="white" />
-      </MoreProjects>
     </div>
   );
 };
