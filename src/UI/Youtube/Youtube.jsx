59c916a4ab906ce6339a578185cc00d01e957578
itacademyeldana <maksatbekovaeldana401@gmail.com>
2025-01-13 22:55:20 +0600

    блок youtube с модальным окном

---
+++ src/UI/Youtube/Youtube.jsx
@@ -0,0 +1,71 @@
+import { useEffect } from "react";
+import { useModal } from "utils/hooks/useModal";
+import PropTypes from "prop-types";
+import styles from "UI/Youtube/Youtube.module.scss";
+import { Heading } from "..";
+
+export const VideoBlock = ({ videoId, backgroundColor }) => {
+  const { isOpen, openModal, closeModal } = useModal();
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add(styles.no_scroll);
+    } else {
+      document.body.classList.remove(styles.no_scroll); 
+    }
+
+    return () => document.body.classList.remove(styles.no_scroll); 
+  }, [isOpen]);
+
+  const colorOptions = {
+    white: "white",
+    black: "#262626",
+  };
+
+  const videoContainerStyle = {
+    backgroundColor: colorOptions[backgroundColor] || "transparent",
+  };
+
+  return (
+    <div className={styles.video_container} style={videoContainerStyle}>
+      <Heading text="YouTube" />
+
+      <div className={styles.video_preview} onClick={openModal}>
+        <iframe
+          src={`https://www.youtube.com/embed/${videoId}?controls=1&autoplay=0`}
+          title="YouTube Video"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        <div className={styles.clickable_overlay}></div>
+      </div>
+
+      {isOpen && (
+        <div className={styles.modal} onClick={closeModal}>
+          <div
+            className={styles.modalContent}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <span className={styles.closeButton} onClick={closeModal}>
+              &times;
+            </span>
+            <iframe
+              src={`https://www.youtube.com/embed/${videoId}?controls=1&autoplay=1`}
+              title="YouTube Video"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              width="900px"
+              height="500px"
+            ></iframe>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+VideoBlock.propTypes = {
+  videoId: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.oneOf(["white", "black"]),
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import {WeInNumber} from "UI/WeInNumber/WeInNumber.jsx";
+import { VideoBlock } from "UI/Youtube/Youtube";
 
 export const HomePage = () => {
 
   return (
       <div>
       <WeInNumber/>
+      <VideoBlock videoId="eA0lHNZ1KCA" backgroundColor="black" />
       </div>
   );
 };
