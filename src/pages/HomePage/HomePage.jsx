f8d56f76c486502f7ec3bfc12c3a58f762aa3c52
itacademyeldana <maksatbekovaeldana401@gmail.com>
2025-01-14 05:08:01 +0600

    сделала стартовый блок

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,14 @@
 import {WeInNumber} from "UI/WeInNumber/WeInNumber.jsx";
+import MainBanner  from "modules/MainBanner/MainBanner";
 
 export const HomePage = () => {
 
   return (
+    <div>
+      <MainBanner/>
       <WeInNumber/>
+
+    </div>
   );
 };
 
