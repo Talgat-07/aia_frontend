11ef20fdf91e68237aff1e7456f71080f75eae2e
Joelwine <stimdlastim@gmail.com>
2025-01-14 17:35:28 +0600

    fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,14 @@
-import {WeInNumber} from "UI/WeInNumber/WeInNumber.jsx";
-import { YoutubeBlock} from "UI/YoutubeBlock/YoutubeBlock.jsx";
+import {WeInNumber, YoutubeBlock} from "UI/index.js";
+import {InformationUs} from "modules/HomePageModules/InformationUs/InformationUs.jsx";
 
 export const HomePage = () => {
 
   return (
       <>
-
+          <InformationUs
+              subtitle="Архитектурно-дизайнерская студия АЙА"
+              description="Lorem ipsum dolor sit amet consectetur. Mi tristique risus accumsan morbi. Sed adipiscing libero vel enim justo phasellus a et. Placerat ultricies ultricies rutrum blandit et arcu. Feugiat arcu turpis interdum tortor eu ut diam tincidunt.Lorem ipsum dolor sit amet     consectetur. Mi tristique risus accumsan morbi. Sed a   dipiscing libero vel enim justo phasellus a et. Placerat ultricies ultricies rarcu turpis interdum tortor eu ut diam tincidunt.Lorem ipsum dolor sit  ut diam tincidunt.Lorem ipsum dolor sitetur. Mi tristique risus accumsan morbi."
+          />
           <WeInNumber/>
           <YoutubeBlock videoId="eA0lHNZ1KCA" backgroundColor="black" />
       </>
