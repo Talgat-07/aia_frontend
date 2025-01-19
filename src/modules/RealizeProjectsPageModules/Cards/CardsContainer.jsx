90ea68566e0cedf993e3ba03d730c5997d9a6e98
itacademyeldana <maksatbekovaeldana401@gmail.com>
2025-01-19 14:00:20 +0600

    кастомные карточки

---
+++ src/modules/RealizeProjectsPageModules/Cards/CardsContainer.jsx
@@ -0,0 +1,52 @@
+import {MainCards} from "UI/MainCards/MainCards"
+import styles from "modules/RealizeProjectsPageModules/CardsContainer/CardsContainer.module.scss";
+import img from "assets/img/RealProjects.png"
+
+export const CardContainer = () => {
+  const cardsData = [
+    {
+      image: img,
+      title: "Заголовок",
+      text: "Коттеджные поселки",
+    },
+    {
+      image: img,
+      title: "Заголовок",
+      text: "Коттеджные поселки",
+    },
+    {
+      image: img,
+      title: "Заголовок",
+      text: "Коттеджные поселки",
+    },
+    {
+      image: img,
+      title: "Заголовок",
+      text: "Коттеджные поселки",
+    },
+    {
+      image: img,
+      title: "Заголовок",
+      text: "Коттеджные поселки",
+    },
+    {
+      image: img,
+      title: "Заголовок",
+      text: "Коттеджные поселки",
+    },
+  ];
+
+  return (
+    <div className={styles.card_container}>
+      {cardsData.map((card, index) => (
+        <MainCards
+          key={index}
+          image={card.image}
+          title={card.title}
+          text={card.text}
+        />
+      ))}
+    </div>
+  );
+};
+
