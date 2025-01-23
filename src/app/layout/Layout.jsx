ef2aaa28fa9e7a988c089903051563828eecbcb4
Aliev Madjit <madzitaliev51@gmail.com>
2025-01-23 22:43:00 +0600

    Fix all header

--- src/app/layout/Layout.jsx
+++ src/app/layout/Layout.jsx
@@ -1,23 +1,22 @@
 import { Footer } from 'modules/Footer/Footer';
-import {Header} from 'modules/Header/Header';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import {Loader} from "UI/index.js";
-import {ScrollToTop} from "utils/helpers/ScrollToTop.js";
+import { Loader } from 'UI/index.js';
 import { ContactForm } from 'UI/ContactForm/ContactForm';
-
+import { ScrollToTop } from 'UI/scrollToTop/ScrollToTop';
+import { Header } from 'modules/headerModule/Components';
 
 export const Layout = () => {
-    return (
-        <>
-            <ScrollToTop/>
-            <Header />
+   return (
+      <>
+         <ScrollToTop />
+         <Header />
 
-            <Suspense fallback={<Loader />}>
-                <Outlet />
-            </Suspense>
-            <ContactForm/>
-            <Footer />
-        </>
-    );
+         <Suspense fallback={<Loader />}>
+            <Outlet />
+         </Suspense>
+         <ContactForm />
+         <Footer />
+      </>
+   );
 };
