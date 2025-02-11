067e2ed596b2882c9164a5c9ea80465e9fb1a66b
Joelwine <stimdlastim@gmail.com>
2025-02-11 20:46:11 +0600

    fix

--- src/UI/Cards/MainCards/MainCards.jsx
+++ src/UI/Cards/MainCards/MainCards.jsx
@@ -1,5 +1,5 @@
 import { Typography } from "UI";
-import styles from "UI/Cards/MainCards/MainCards.module.scss";
+import styles from "./MainCards.module.scss";
 
 export const MainCards = ({ image, title, text }) => {
   return (
--- src/UI/CustomBlogGallery/CustomBlogGallery.jsx
+++ src/UI/CustomBlogGallery/CustomBlogGallery.jsx
@@ -1,9 +1,9 @@
-import { BlogCards } from '..';
+import {BlogCards, Container} from '..';
 import styles from './CustomBlogGallery.module.scss';
 
 export const CustomBlogGallery = ({ cardsData }) => {
    return (
-      <div>
+      <Container>
          <div className={styles.galleryBlock}>
             {cardsData.map((card, index) => (
                <BlogCards
@@ -16,6 +16,6 @@ export const CustomBlogGallery = ({ cardsData }) => {
                />
             ))}
          </div>
-      </div>
+      </Container>
    );
 };
